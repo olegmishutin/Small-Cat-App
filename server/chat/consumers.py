21dc050ef1e561c69a840956c0b8d5b1d620c96c
olegmishutin <omisutin646@gmail.com>
2024-10-04 23:07:05 +0300

    fixed

--- server/chat/consumers.py
+++ server/chat/consumers.py
@@ -48,6 +48,12 @@ class ChatConsumer(AsyncWebsocketConsumer):
                 }
             )
 
+    async def disconnect(self, code):
+        await self.channel_layer.group_discard(
+            self.room_group_name,
+            self.channel_name
+        )
+
     async def chat_message(self, event):
         await self.send(text_data=json.dumps({
             'message': event['message'],
