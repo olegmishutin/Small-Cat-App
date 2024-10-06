8d88b817af57e13e6df37aa2a74e2efc54e82b8a
olegmishutin <omisutin646@gmail.com>
2024-10-06 12:48:42 +0300

    added client with login and register page

---
+++ client/src/app/login/login.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-login',
+  standalone: true,
+  imports: [],
+  templateUrl: './login.component.html',
+  styleUrl: '../../styles/auth_style.css'
+})
+export class LoginComponent {
+
+}
---
+++ client/src/app/register/register.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-register',
+  standalone: true,
+  imports: [],
+  templateUrl: './register.component.html',
+  styleUrl: '../../styles/auth_style.css'
+})
+export class RegisterComponent {
+
+}
